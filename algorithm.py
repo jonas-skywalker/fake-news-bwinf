f31050b64df6a97b25448d9d1c3ea2be36ad5dc9
Marc Thieme <zonico67@gmail.com>
2020-02-06 16:28:17 +0100

    added output

--- algorithm.py
+++ algorithm.py
@@ -1,23 +1,19 @@
 def tap_network_of_trumpf(gr):
-    by_connectivity = ordered_connectivity_list(gr)
+    connectivities = connectivity_list(gr)
     tapped = set()
-    while by_connectivity:
-        (vertex, connectivity) = by_connectivity[0]
+    while connectivities:
+        (index, (vertex, connectivity)) = max(enumerate(connectivities), key=lambda x: x[1][1])
         if not connectivity:
             break
 
-        del by_connectivity[0]
+        del connectivities[index]
 
         tapped.add(vertex)
-        for (index, (v, neighs)) in enumerate(by_connectivity):
+        for (index, (v, neighs)) in enumerate(connectivities):
             if gr.is_neighbour(v, vertex):
-                by_connectivity[index] = (v, neighs - 1)
+                connectivities[index] = (v, neighs - 1)
 
-        by_connectivity.sort(key=lambda x: x[1], reverse=True)
     return tapped
 
-def ordered_connectivity_list(graph):
-    with_conn = list()
-    for vertex, neighs in graph.node_neighbour_pairs():
-        with_conn.append((vertex, len(neighs)))
-    return sorted(with_conn, key=lambda x: x[1], reverse=True)
+def connectivity_list(graph):
+    return list(map(lambda x: (x[0], len(x[1])), graph.node_neighbour_pairs()))
--- main.py
+++ main.py
@@ -2,7 +2,17 @@ import graph
 import algorithm
 import time
 
-print("test")
-start = time.time()
-gr = graph.Graph.create_graph(r".\jugendforumInformatik2020\problem\input\hard_5.graph")
-print(len(algorithm.tap_network_of_trumpf(gr)), time.time() - start)
+outdirectory = r".\output"
+gr = graph.Graph.create_graph(r".\jugendforumInformatik2020\problem\input\hard_1.graph")
+for i in range(1, 6):
+    for prefix in ["easy", "medium", "hard"]:
+        file_name = r".\jugendforumInformatik2020\problem\input" + "\\" + prefix + "_" + str(i) + ".graph"
+        gr = graph.Graph.create_graph(file_name)
+        tapped = list(algorithm.tap_network_of_trumpf(gr))
+        length = len(tapped)
+        filecontent = str(length) + "\n"
+        for node in tapped:
+            filecontent += str(node) + " "
+        filecontent = filecontent[:-1]
+        with open(outdirectory + "\\" + prefix + "_" + str(i) + ".sol", "w") as outfile:
+            outfile.write(filecontent)
