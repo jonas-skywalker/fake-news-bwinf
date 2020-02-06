bf8f75f81cdb446bacdfc0a45c349f636b25433b
Marc Thieme <zonico67@gmail.com>
2020-02-06 13:28:18 +0100

    Unified parsing and greating graphs

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 class Graph:
-    def parse_notes(self, file):
-        with open(file, "r") as infile:
-            input = infile.readlines()
-            head = input[0].split()
-            n = head[2]
-            m = head[3]
-
-            values = input[1:]
-            nodes = list()
-
-            for line in values:
-                line_list = line.split()
-                nodes.append(tuple(map(int, line_list)))
-
-            return nodes
-
     def __init__(self, nodes):
         self.nodes = nodes
+
+    def create_graph(file):
+        edges = parse_notes(file)
+        nodes_dict = dict()
+        for (v, u) in edges:
+            if v not in nodes_dict:
+                nodes_dict[v] = []
+            if u not in nodes_dict:
+                nodes_dict[u] = []
+            nodes_dict[v].append(u)
+            nodes_dict[u].append(v)
+        return Graph(nodes_dict)
 
     def neighbours(self, v):
         return self.nodes[v]
@@ -32,14 +28,20 @@ class Graph:
         self.nodes[v].append(u)
         self.nodes[u].append(v)
 
+def parse_notes(file):
+    with open(file, "r") as infile:
+        input = infile.readlines()
+        head = input[0].split()
+        n = head[2]
+        m = head[3]
 
-def create_graph(edges):
-    nodes_dict = dict()
-    for (v, u) in edges:
-        if v not in nodes_dict:
-            nodes_dict[v] = []
-        if u not in nodes_dict:
-            nodes_dict[u] = []
-        nodes_dict[v].append(u)
-        nodes_dict[u].append(v)
-    return Graph(nodes_dict)
+        values = input[1:]
+        nodes = list()
+
+        for line in values:
+            line_list = line.split()
+            nodes.append(tuple(map(int, line_list)))
+
+    return nodes
+
+print(create_graph(r".\jugendforumInformatik2020\problem\input\easy_1.graph").nodes)
