42d9a12f91c53707aa5a180412062df3d2ac2e66
Marc Thieme <zonico67@gmail.com>
2020-02-06 13:14:43 +0100

    Constructing Graph from/adding edges

--- main.py
+++ main.py
@@ -1,3 +1,37 @@
 class Graph:
-    def __init__(self):
-        self.nodes = dict()
+    def __init__(self, nodes):
+        self.nodes = nodes
+
+    def neighbours(self, v):
+        return self.nodes[v]
+
+    def ensure(self, *vs):
+        for v in vs:
+            if v not in self.nodes:
+                self.nodes[v] = []
+
+    def add_edge(self, edge):
+        (u, v) = edge
+        self.ensure(u, v)
+        self.nodes[v].append(u)
+        self.nodes[u].append(v)
+
+def create_graph(edges):
+    nodes_dict = dict()
+    for (v, u) in edges:
+        if v not in nodes_dict:
+            nodes_dict[v] = []
+        if u not in nodes_dict:
+            nodes_dict[u] = []
+        nodes_dict[v].append(u)
+        nodes_dict[u].append(v)
+    return Graph(nodes_dict)
+
+print(create_graph([(1,2), (2,3), (6,3)]).nodes)
+
+infile = "23432 543435\n432423 543"
+ret = []
+for i in infile.split('\n'):
+    parts = i.split()
+    ret.append(tuple(map(int, parts)))
+print(ret)
