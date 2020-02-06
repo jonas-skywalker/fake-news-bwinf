78461df1383fcc3c6ffffbc0b0985f55b577fcd6
jonas <jonasbiba@tutanota.com>
2020-02-06 14:18:26 +0100

    Adding Ordering function

--- algorithm.py
+++ algorithm.py
@@ -2,3 +2,8 @@ def tap_network_of_trumpf(graph):
     by_connectivity = ordered_connectivity_list(graph)
 
 def ordered_connectivity_list(graph):
+    with_conn = list()
+    for vertex in graph.nodes:
+        with_conn.append((vertex, len(graph.neighbours(vertex))))
+    return sorted(with_conn, key=lambda x: x[1], reverse=True)
+
